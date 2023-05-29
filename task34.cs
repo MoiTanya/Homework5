8bcf92eafac8cac9dfb32e3660b2f90f7571aa7d
MoiTanya <130609745+MoiTanya@users.noreply.github.com>
2023-05-29 19:56:05 +0300

    Add files via upload
    task34 and task36 commit

---
+++ task34.cs
@@ -0,0 +1,29 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int EvenNumber (int[] array)
+{
+    int count =  0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]%2==0) count++;
+    }
+    return count;
+}
+
+int[] RandomArray (int size)
+{   
+    int[] Array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(1000);
+    }
+    return Array;
+}
+
+Console.WriteLine("Введите размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] NewArray = RandomArray(sizeArray);
+int countEven = EvenNumber(NewArray);
+Console.WriteLine(String.Join(',', NewArray)+$"-> {countEven}");
---
+++ task36.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] RandomArray (int sizeArr, int sizeElem)
+{   
+    int[] Array = new int[sizeArr];
+    for (int i = 0; i < sizeArr; i++)
+    {
+        Array[i] = new Random().Next(sizeElem);
+    }
+    return Array;
+}
+
+int SumOddNumber (int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i+1)%2!=0)
+        summ = summ + array[i];
+    }
+    return summ;
+}
+
+Console.WriteLine("Введите размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размерность элементов массива");
+int sizeElement = Convert.ToInt32(Console.ReadLine());
+int[] NewArray = RandomArray(sizeArray, sizeElement);
+int summ = SumOddNumber(NewArray);
+Console.WriteLine(String.Join(',', NewArray)+$"-> {summ}");
